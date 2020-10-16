5455a10b97f7bc39154c38dd422dc720d2f07fae
cccc <cuiwei_cv@163.com>
2020-10-16 13:55:09 +0800

    use microlib and add printf from uart0

--- test_gd32/App/main.c
+++ test_gd32/App/main.c
@@ -18,7 +18,10 @@
 #include "gd32f1x0.h"
 #include "gd32f1x0_libopt.h"
 #include "systick.h"
+
 #include "led.h"
+#include "usart.h"
+#include <stdio.h>  //printf等
 
 
 /**
@@ -30,14 +33,23 @@
 int main(void)
 {
 	systick_config();	//初始化滴答定时器
-	
 	LED_Init();	//LED初始化
+  USART0_Init();	//串口0初始化
 	
 	while(1)
 	{
+    printf("Hello World!\r\n");	//串口打印：Hello World!
 		LED1_Toggle();	//对LED1的状态进行取反
 		delay_1ms(500);
 	}
+}
+
+/* 重定向C库中的printf函数 */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART0, (uint8_t)ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
 }
 
 
